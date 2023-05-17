f8eebb462beb3c19faac2765a2bdd2b76483966b
adhamcahyo <127497339+adhamcahyo@users.noreply.github.com>
2023-05-17 20:59:33 +0700

    destruktor.cpp

--- destruktor.cpp
+++ destruktor.cpp
@@ -5,3 +5,38 @@ class angka{
     private:
     int *arr;
     int panjang;
+
+    public:
+    angka(int);
+    ~angka();
+    void cetakData();
+    void isiData();
+};
+
+angka::angka(int i){
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka(){
+    cout<<endl;
+    cetakData();
+    delete[]arr;
+    cout<< "Alamat Array Sudah Dilepaskan"<<endl;
+}
+
+void angka::cetakData(){
+    for(int i=1;i<=panjang;i++){
+        cout<<i<<" = "<<arr[i]<<endl;
+    }
+    cout<<endl;
+}
+
+int main(){
+    angka belajarcpp(3);
+    angka *ptrBelajarcpp = new angka(5);
+    delete ptrBelajarcpp;
+
+    return 0;
+}
